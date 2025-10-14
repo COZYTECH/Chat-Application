c108f6d4c7ec458df03eef906e3f949fcbc50e09
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2025-10-14 11:22:04 +0100

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running ");
+});
