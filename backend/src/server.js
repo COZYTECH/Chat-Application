b2545dc5c45e34c6c117224adc322df0d4f6ab14
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2025-10-14 14:28:34 +0100

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoute.js";
 import path from "path";
+import { connectDb } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
+app.use(express.json());
 app.use("/v1/api/auth", authRoutes);
 
 //making it ready for deployment
@@ -20,4 +22,5 @@ if (process.env.NODE_ENV === "production") {
 app.listen(process.env.PORT, () => {
   console.log("Server is running ");
   console.log(`http://localhost:${process.env.PORT}`);
+  connectDb();
 });
