661c44160cb428c552f95ef32a83875cc71732d9
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2025-10-15 11:49:52 +0100

    update profile upload endpoint

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { signUp, signIn, logout } from "../controller/authController.js";
+import {
+  signUp,
+  signIn,
+  logout,
+  updateProfile,
+} from "../controller/authController.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/logout", logout);
+router.put("/updateprofile", protectRoute, updateProfile);
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
 
 export default router;
